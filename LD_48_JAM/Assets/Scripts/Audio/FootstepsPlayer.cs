771dc57b73964331ee8166a25c123b3cbb6d2dec
Pat Cleaver <pat.cleaver.bass@gmail.com>
2021-04-25 12:36:50 +0200

    added silly footsteps

--- LD_48_JAM/Assets/Scripts/Audio/FootstepsPlayer.cs
+++ LD_48_JAM/Assets/Scripts/Audio/FootstepsPlayer.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class FootstepsPlayer : MonoBehaviour
 {
-public void PlayFootstep()
+
+    public void PlayFootstep()
     {
         FMODUnity.RuntimeManager.PlayOneShotAttached("event:/Footsteps", gameObject);
+    }
+
+    public void PlayHighHeelFootstep()
+    {
+        FMODUnity.RuntimeManager.PlayOneShotAttached("event:/HighHeelFootsteps", gameObject);
+    }
+
+    public void PlayPizzFootstep()
+    {
+        FMODUnity.RuntimeManager.PlayOneShotAttached("event:/PizzFootsteps", gameObject);
     }
 }
