80482b16d1e4d5a35a78468f8942ea37cbfc795e
Pat Cleaver <pat.cleaver.bass@gmail.com>
2021-04-26 19:14:58 +0200

    added public functions to call and trigger music stuff

--- LD_48_JAM/Assets/Scripts/Audio/MusicSystemPlayer.cs
+++ LD_48_JAM/Assets/Scripts/Audio/MusicSystemPlayer.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MusicSystemPlayer : MonoBehaviour
 {
 
+    //make this a singleton
+
     FMOD.Studio.EventInstance music;
     public int panicForMusic = 0;
 
@@ -20,6 +22,24 @@ public class MusicSystemPlayer : MonoBehaviour
     void Update()
     {
         music.setParameterByName("Panic", panicForMusic, false);
+    }
+
+    public void SetMenuMusic()
+    {
+        music.setParameterByName("Menu", 1, false);
+    }
+
+    public void StartLevelMusic()
+    {
+        music.setParameterByName("Menu", 0, false);
+        music.setParameterByName("Panic", 0, false);
+        music.setParameterByName("YouLose", 0, false);
+    }
+
+    public void StartYouLoseMusic()
+    {
+        music.setParameterByName("YouLose", 1, false);
+        music.setParameterByName("Menu", 1, false);
     }
 
     private void OnDestroy()
