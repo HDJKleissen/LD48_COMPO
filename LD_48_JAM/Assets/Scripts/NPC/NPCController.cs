9cad3d43d1dcfcc67d0c2eb4a3ef63e3278ec8e6
Pat Cleaver <pat.cleaver.bass@gmail.com>
2021-04-27 00:46:30 +0200

    trigger various barks

--- LD_48_JAM/Assets/Scripts/NPC/NPCController.cs
+++ LD_48_JAM/Assets/Scripts/NPC/NPCController.cs
@@ -101,9 +101,18 @@ public class NPCController : MonoBehaviour
                 {
                     FMODUnity.RuntimeManager.PlayOneShotAttached("event:/NpcCactusBark", gameObject);
                 }
-                else
+                else if (GameManager.Instance.Player.Disguise == PlayerDisguise.Woman ||
+                    GameManager.Instance.Player.Disguise == PlayerDisguise.WomanBlack ||
+                    GameManager.Instance.Player.Disguise == PlayerDisguise.WomanDress)
                 {
-                    FMODUnity.RuntimeManager.PlayOneShotAttached("event:/NpcDetectedBark", gameObject);
+                    FMODUnity.RuntimeManager.PlayOneShotAttached("event:/NpcRecogniseFemaleDisguiseBark", gameObject);
+                }
+                else if (GameManager.Instance.Player.Disguise == PlayerDisguise.Man ||
+                    GameManager.Instance.Player.Disguise == PlayerDisguise.ManBald ||
+                    GameManager.Instance.Player.Disguise == PlayerDisguise.ManTie ||
+                    GameManager.Instance.Player.Disguise == PlayerDisguise.Security)
+                {
+                    FMODUnity.RuntimeManager.PlayOneShotAttached("event:/NpcRecogniseMaleDisguiseBark", gameObject);
                 }
             }
 
